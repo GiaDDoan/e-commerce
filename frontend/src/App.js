552838ea1437c65ad32df0bd8bc2aed418c6bc6a
Gia Dat Doan <giadatdoan@gmail.com>
2021-05-04 03:14:59 -0400

    added router and started header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/home/Home';
 
 function App() {
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
