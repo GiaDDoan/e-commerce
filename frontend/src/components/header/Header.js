60e6b821dde79417da56ef72b60cf55fdc5b7ce3
Gia Dat Doan <giadatdoan@gmail.com>
2021-05-04 05:14:54 -0400

    added styled components

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import './Header.css';
+
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div>
+        <input className="header_searchBar" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div>
+        <div className="header_signIn">
+          <span>Sign In</span>{' '}
+        </div>
+        <div className="header_basket">
+          <ShoppingBasket />
+          <span>0</span>
+        </div>
+      </div>
+    </div>
+  );
 }
