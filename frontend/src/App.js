bafa55b86177ae057a6f49e7b359e3b43171e09d
Gia Dat Doan <giadatdoan@gmail.com>
2021-05-04 03:59:40 -0400

    added header to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Header from './components/header/Header';
 import Home from './components/home/Home';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
