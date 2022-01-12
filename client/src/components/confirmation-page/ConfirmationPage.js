eb1e6e8e0f4ed987fe3487e7d0bec9f0835b3cdf
gia <giadatdoan@gmail.com>
2022-01-12 14:11:18 -0500

    confirm page almost

--- client/src/components/confirmation-page/ConfirmationPage.js
+++ client/src/components/confirmation-page/ConfirmationPage.js
@@ -5,14 +5,21 @@ import { BsFillCartCheckFill } from "react-icons/bs";
 const ConfirmationPage = ({ confirmationName }) => {
   console.log("as", confirmationName);
   const confirmationNumber = Date.now();
+  // confirmationName = "da";
 
   return (
     <div className="confirm-wrapper">
       <div className="confirm-ty">
-        Thank You {confirmationName} for ordering!
+        <div className="confirm-logo-wrapper">
+          <BsFillCartCheckFill className="confirm-logo" />
+        </div>
+        <div className="confirm-note">
+          Thank you {confirmationName} for placing an ordering!
+        </div>
       </div>
       <div className="confirm-number">
-        Your confirmation number is: {confirmationNumber}
+        <h4>Confirmation Number:</h4>
+        <div>{confirmationNumber}</div>
       </div>
     </div>
   );
