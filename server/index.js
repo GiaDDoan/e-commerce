02f62d17edd70ddc01e2cf77442f3849a7831d73
gia <giadatdoan@gmail.com>
2022-01-13 04:08:53 -0500

    adding heroku

--- server/index.js
+++ server/index.js
@@ -18,8 +18,6 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => console.log("db connected"));
 
-const PORT = 5000;
-
 app
   // .use(cors())
   .use(express.urlencoded({ extended: false }))
@@ -30,4 +28,7 @@ app
   .use("/api", userRoute)
   .use("/search", searchRoute)
 
-  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+  // .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+  .listen(process.env.PORT || 5000, () =>
+    console.info(`Listening on port ${PORT}`)
+  );
